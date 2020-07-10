7206b7ff3d27bf8094575682abdf0bf986ca5b3f
Dimitri <d.kouliche@gmail.com>
2020-07-10 21:09:27 +0200

    Add game over

---
+++ Assets/Scripts/BallCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour
+{
+    public GameObject gameOver;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Ground")
+        {
+            StartCoroutine(GameOverCoroutine());
+        }
+    }
+    IEnumerator GameOverCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        gameOver.SetActive(true);
+    }
+}
