c854a6a7c47f2219023cfc45102e188359a4e015
Dimitri <d.kouliche@gmail.com>
2020-07-11 14:08:48 +0200

    Add ball indicator

---
+++ Assets/Scripts/BallLogics.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BallLogics : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject ballIndicator;
+    public GameObject gameLogics;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Left Ground" && gameLogics.GetComponent<GameLogics>().isPlaying)
+        {
+            gameLogics.GetComponent<GameLogics>().PlayerWins("Blob 2");
+        }
+
+        if(collision.gameObject.name == "Right Ground" && gameLogics.GetComponent<GameLogics>().isPlaying)
+        {
+            gameLogics.GetComponent<GameLogics>().PlayerWins("Blob 1");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ballIndicator.activeSelf)
+        {
+            ballIndicator.transform.position = new Vector3 (ball.transform.position.x, 4.5f, -2);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        ballIndicator.SetActive(true);
+    }
+
+    void OnBecameVisible()
+    {
+        ballIndicator.SetActive(false);
+    }
+}
--- Assets/Scripts/GameLogics.cs
+++ Assets/Scripts/GameLogics.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameLogics : MonoBehaviour
 {
     public GameObject uiMessage;
+    public GameObject uiScore;
     public GameObject gameOver;
     public GameObject blob1;
     public GameObject blob2;
@@ -13,37 +14,45 @@ public class GameLogics : MonoBehaviour
     
     Vector3 blob1Position;
     Vector3 blob2Position;
+    Vector3 blob1Scale;
+    Vector3 blob2Scale;
     Vector3 ballPosition;
+    int blob1Score = 0;
+    int blob2Score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         blob1Position = blob1.transform.position;
         blob2Position = blob2.transform.position;
+        blob1Scale = blob1.transform.localScale;
+        blob2Scale = blob2.transform.localScale;
         ballPosition = ball.transform.position;
-        beginGame();
+        BeginGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return) && isPlaying == true)
         {
             Time.timeScale = 1;
             uiMessage.SetActive(false);
         }
     }
 
-    void beginGame()
+    void BeginGame()
     {
         Time.timeScale = 0;
         isPlaying = true;
-        updateMessage("Press Enter to begin round");
+        UpdateMessage("Press Enter to begin round");
     }
 
-    public void resetPositions(string winner)
+    public void ResetPositions(string winner)
     {
-        if(winner == "Blob 1")
+        ball.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        ball.GetComponent<Rigidbody2D>().angularVelocity = 0;
+        if (winner == "Blob 1")
         {
             ball.transform.position = new Vector3(-ballPosition.x, ballPosition.y);
         } else
@@ -52,32 +61,53 @@ public class GameLogics : MonoBehaviour
         }
         blob1.transform.position = blob1Position;
         blob2.transform.position = blob2Position;
+        blob1.transform.localScale = blob1Scale;
+        blob2.transform.localScale = blob2Scale;
     }
 
-    public void updateMessage(string message)
+    public void UpdateMessage(string message)
     {
         uiMessage.SetActive(true);
-        uiMessage.transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Text>().text = message;
+        uiMessage.GetComponent<Text>().text = message;
     }
 
-    public void playerWins(string player)
+    public void DisplayScore()
     {
+        uiScore.GetComponent<Text>().text = blob1Score + " - " + blob2Score;
+    }
+
+    public void PlayerWins(string player)
+    {
+        if (player == "Blob 1") {
+            blob1Score++;
+        } else
+        {
+            blob2Score++;
+        }
+        DisplayScore();
         isPlaying = false;
-        updateMessage(player + " wins the round");
+        if(blob1Score >= 15 || blob2Score >= 15)
+        {
+            UpdateMessage("CONGRATULATIONS, " + player + ", you have won the GAME");
+            StartCoroutine(GameOverCoroutine());
+            return;
+        }
+        UpdateMessage(player + " wins the round");
         StartCoroutine(PlayerWinsCorountine(player));
     }
 
     IEnumerator PlayerWinsCorountine(string player)
     {
         yield return new WaitForSeconds(3);
-        resetPositions(player);
+        ResetPositions(player);
         eraseMessage();
-        beginGame();
+        BeginGame();
     }
 
     IEnumerator GameOverCoroutine()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(3);
+        eraseMessage();
         gameOver.SetActive(true);
     }
 
