83f1265e0bcf0ecc738595853fbfd2d81749b94c
Dimitri <d.kouliche@gmail.com>
2020-07-11 17:19:14 +0200

    Make blob eyes follow the ball

---
+++ Assets/Scripts/FollowBall.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FollowBall : MonoBehaviour
+{
+    public GameObject ball;
+
+    Vector3 heading;
+
+    // Update is called once per frame
+    void Update()
+    {
+        heading = ball.transform.position - transform.position;
+        if (heading.x > 1)
+        {
+            heading.x = 1;
+        }
+        if(heading.x < -1)
+        {
+            heading.x = -1;
+        }
+        if (heading.y > 1)
+        {
+            heading.y = 1;
+        }
+        if (heading.y < -1)
+        {
+            heading.y = -1;
+        }
+        transform.localPosition = new Vector3(heading.x * 0.05f, heading.y * 0.05f - 0.05f, -3);
+    }
+}
