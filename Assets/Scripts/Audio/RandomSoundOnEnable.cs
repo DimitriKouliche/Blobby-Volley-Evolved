04b91897533e463bbfe5c4986d11105422684405
Dimitri <d.kouliche@gmail.com>
2020-08-25 12:55:07 +0200

    Add dash sound

---
+++ Assets/Scripts/Audio/RandomSoundOnEnable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RandomSoundOnEnable : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip[] clips;
+
+    private void OnEnable()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        int index = Random.Range(0, clips.Length);
+        audioSource.clip = clips[index];
+        audioSource.Play();
+    }
+
+}
