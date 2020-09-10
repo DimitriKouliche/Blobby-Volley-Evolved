bf334a539fa280416ab4c6f89e5c2e4e6ef352ed
Dimitri <d.kouliche@gmail.com>
2020-09-10 12:12:49 +0200

    Finish main menu

--- Assets/Scripts/Gameplay/MainMenuController.cs
+++ Assets/Scripts/Gameplay/MainMenuController.cs
@@ -9,19 +9,38 @@ public class MainMenuController : MonoBehaviour
     public GameObject[] arrows;
     public GameObject soundManager;
     public GameObject credits;
+    public GameObject duelSplash;
+    public GameObject partySplash;
+    public GameObject keyboardControls;
+    public GameObject gamepadControls;
+    public GameObject soundMenu;
     public string[] scenes;
     int menuId = 0;
     PlayerInput playerInput;
     InputAction moveAction;
     InputAction confirmAction;
     bool inputOnCooldown = false;
-    bool splashScreen = true;
+    bool splashScreen = false;
     bool isOnControl = false;
     bool isOnSound = false;
+    bool isOnGamePad = true;
+    bool isOnKeyboard = false;
+    bool isOnMusic = false;
+    bool isOnSFX = false;
+    float musicVolume;
+    float sfxVolume;
+    Vector3 SFXJaugeOrigin = new Vector3(0.19f, -6.94f, 0);
+    Vector3 SFXJaugeDestination = new Vector3(0.22f, -15.87f, 0);
+    Vector3 musicJaugeOrigin = new Vector3(0.58f, -7f, 0);
+    Vector3 musicJaugeDestination = new Vector3(0.61f, -16.61f, 0);
 
     // Use this for initialization
     void Start()
     {
+        musicVolume = PlayerPrefs.GetFloat("musicVolume", 100f);
+        sfxVolume = PlayerPrefs.GetFloat("sfxVolume", 100f);
+        FindChild(FindChild(soundMenu, "ABoutonSFX"), "SFXJaugeA").transform.localPosition = Vector3.Lerp(SFXJaugeOrigin, SFXJaugeDestination, 1 - sfxVolume / 100f);
+        FindChild(FindChild(soundMenu, "ABoutonMusic"), "MusicJaugeA").transform.localPosition = Vector3.Lerp(musicJaugeOrigin, musicJaugeDestination, 1 - musicVolume / 100f);
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
         confirmAction = playerInput.actions["Confirm"];
@@ -80,14 +99,87 @@ public class MainMenuController : MonoBehaviour
             inputOnCooldown = true;
             StartCoroutine(EnableInput(0.15f));
         }
+        if (moveDirectionVector.x > 0.9f)
+        {
+            HandleXMovement(true);
+            inputOnCooldown = true;
+            StartCoroutine(EnableInput(0.15f));
+        }
+        if (moveDirectionVector.x < -0.9f)
+        {
+            HandleXMovement(false);
+            inputOnCooldown = true;
+            StartCoroutine(EnableInput(0.15f));
+        }
     }
+
+    void HandleXMovement(bool isRight)
+    {
+        if(isOnControl)
+        {
+            if(isOnGamePad && isRight)
+            {
+                isOnGamePad = false;
+                isOnKeyboard = true;
+            }
+            else if (isOnKeyboard && !isRight)
+            {
+                isOnGamePad = true;
+                isOnKeyboard = false;
+            }
+            keyboardControls.SetActive(isOnKeyboard);
+            gamepadControls.SetActive(isOnGamePad);
+        }
+        if(isOnSound)
+        {
+            if(!isOnMusic && !isOnSFX && isRight)
+            {
+                isOnMusic = true;
+            }
+            else if(isOnMusic && isRight)
+            {
+                isOnMusic = false;
+                isOnSFX = true;
+            }
+            else if (isOnSFX && !isRight)
+            {
+                isOnMusic = true;
+                isOnSFX = false;
+            }
+            else if (isOnMusic && !isRight)
+            {
+                isOnMusic = false;
+                isOnSFX = false;
+            }
+            FindChild(FindChild(soundMenu, "ABoutonMusic"), "ParticleSelection").SetActive(isOnMusic);
+            FindChild(FindChild(soundMenu, "ABoutonSFX"), "ParticleSelection").SetActive(isOnSFX);
+        }
+    }
+
 
     void DisplayRightPanel()
     {
         soundManager.SetActive(false);
         credits.SetActive(false);
+        partySplash.SetActive(false);
+        duelSplash.SetActive(false);
+        isOnControl = false;
+        isOnSound = false;
+        if (menuId == 0)
+        {
+            partySplash.SetActive(true);
+        }
+        if (menuId == 1)
+        {
+            duelSplash.SetActive(true);
+        }
+        if (menuId == 2)
+        {
+            isOnControl = true;
+        }
         if (menuId == 3)
         {
+            isOnSound = true;
             soundManager.SetActive(true);
         }
         if (menuId == 4)
@@ -98,6 +190,28 @@ public class MainMenuController : MonoBehaviour
 
     void MoveToNext()
     {
+        if (isOnSFX)
+        {
+            if(sfxVolume <= 0)
+            {
+                return;
+            }
+            sfxVolume -= 5;
+            FindChild(FindChild(soundMenu, "ABoutonSFX"), "SFXJaugeA").transform.localPosition = Vector3.Lerp(SFXJaugeOrigin, SFXJaugeDestination, 1 - sfxVolume / 100f);
+            PlayerPrefs.SetFloat("sfxVolume", sfxVolume);
+            return;
+        }
+        if (isOnMusic)
+        {
+            if (musicVolume <= 0)
+            {
+                return;
+            }
+            musicVolume -= 5;
+            FindChild(FindChild(soundMenu, "ABoutonMusic"), "MusicJaugeA").transform.localPosition = Vector3.Lerp(musicJaugeOrigin, musicJaugeDestination, 1 - musicVolume / 100f);
+            PlayerPrefs.SetFloat("musicVolume", musicVolume);
+            return;
+        }
         if (menuId >= 5)
         {
             return;
@@ -113,8 +227,26 @@ public class MainMenuController : MonoBehaviour
 
     void MoveToPrevious()
     {
-        if (menuId <= 0)
+        if (isOnSFX)
         {
+            if (sfxVolume >= 100)
+            {
+                return;
+            }
+            sfxVolume += 5;
+            FindChild(FindChild(soundMenu, "ABoutonSFX"), "SFXJaugeA").transform.localPosition = Vector3.Lerp(SFXJaugeOrigin, SFXJaugeDestination, 1 - sfxVolume / 100f);
+            PlayerPrefs.SetFloat("sfxVolume", sfxVolume);
+            return;
+        }
+        if (isOnMusic)
+        {
+            if (musicVolume >= 100)
+            {
+                return;
+            }
+            musicVolume += 5;
+            FindChild(FindChild(soundMenu, "ABoutonMusic"), "MusicJaugeA").transform.localPosition = Vector3.Lerp(musicJaugeOrigin, musicJaugeDestination, 1 - musicVolume / 100f);
+            PlayerPrefs.SetFloat("musicVolume", musicVolume);
             return;
         }
         for (int i = 0; i < arrows.Length; i++)
