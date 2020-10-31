7a0b2d32bb3908733906816e29500b11d95f8d87
léonce <lefranciscain@hotmail.com>
2020-10-31 17:59:54 +0100

    Update play modes descriptions

---
+++ Assets/Scripts/Rendering/ChromaticAberrationEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ChromaticAbberationEffect : MonoBehaviour
+{
+    public Volume volume;
+    ChromaticAberration chromatic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume.profile.TryGet<ChromaticAberration>(out chromatic);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chromatic.intensity.value = Mathf.PingPong(Time.time * 6, 1f);
+    }
+}
