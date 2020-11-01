8c98367f506d3d4de589e8ada9bde70f5789849a
léonce <lefranciscain@hotmail.com>
2020-11-02 00:58:07 +0100

    Create chromatic aberration effect script

--- Assets/Scripts/Rendering/ChromaticAberrationEffect.cs
+++ Assets/Scripts/Rendering/ChromaticAberrationEffect.cs
@@ -7,20 +7,32 @@ using UnityEngine.Rendering.Universal;
 public class ChromaticAberrationEffect : MonoBehaviour
 {
     public Volume volume;
+    public float SlowMo = 0.5f;
     ChromaticAberration chromatic;
+   
 
 
     // Start is called before the first frame update
     void Start()
     {
         volume.profile.TryGet<ChromaticAberration>(out chromatic);
+        StartCoroutine(MyUpdate());
     }
 
+
     // Update is called once per frame
-    void Update()
+    IEnumerator MyUpdate()
     {
-        { 
-            chromatic.intensity.value = Mathf.PingPong(Time.time * 6, 1f); 
+        float timer = 0f;
+        float time = 0.69f;
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            chromatic.intensity.value = Mathf.PingPong(Time.time * 3f, 1f);
+            Time.timeScale = SlowMo;
+            yield return null;
         }
+        chromatic.intensity.value = 0f;
+        Time.timeScale = 1f;
     }
 }
